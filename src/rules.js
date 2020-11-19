a2a3412b0f9b8ff8b74ab193c9034098c02f1e41
rhangai <hangai.renan@gmail.com>
2020-11-19 10:13:31 -0300

    chore: Updated vue rules

--- src/rules.js
+++ src/rules.js
@@ -76,5 +76,19 @@ module.exports = {
 	},
 	vue: {
 		'vue/html-indent': ['warn', 'tab'],
+		'vue/max-attributes-per-line': 'off',
+		'vue/singleline-html-element-content-newline': 'off',
+		'vue/html-self-closing': [
+			'error',
+			{
+				html: {
+					void: 'always',
+					normal: 'always',
+					component: 'always',
+				},
+				svg: 'always',
+				math: 'always',
+			},
+		],
 	},
 };
